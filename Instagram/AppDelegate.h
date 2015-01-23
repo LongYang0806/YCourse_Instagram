0abc342c8d58cd3eec14892dd7a4bf08c96a1eba
Long Yang <longyang@yahoo-inc.com>
2015-01-22 19:06:11 -0800

    Initial Commit

---
+++ Instagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Instagram
+//
+//  Created by Long Yang on 1/22/15.
+//  Copyright (c) 2015 Grace Wu/Long Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Instagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Instagram
+//
+//  Created by Long Yang on 1/22/15.
+//  Copyright (c) 2015 Grace Wu/Long Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
